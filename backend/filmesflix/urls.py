5067f64b0a54db73dec89b450d3d0c2fa8b1ac6e
Gabriel Gontijo <gontijogabr@gmail.com>
2023-10-13 18:00:18 -0300

    a

--- backend/filmesflix/urls.py
+++ backend/filmesflix/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from filmesflix.views import FilmesList, FilmesPorID, FilmesEdit, FilmesDelete
+from filmesflix.views import FilmesList, FilmesAdd, FilmesPorID, FilmesEdit, FilmesDelete
 
 urlpatterns = [
     path('filmes/', FilmesList.as_view(), name='filmes-list'),
+    path('filmes/add/', FilmesAdd.as_view(), name='filmes-list'),
     path('filmes/<int:id>/', FilmesPorID.as_view(), name='filmes-id'),
     path('filmes/edit/<int:id>/', FilmesEdit.as_view(), name='filmes-edit'),
     path('filmes/del/<int:id>/', FilmesDelete.as_view(), name='filmes-delete'),

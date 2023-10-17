81091e2a0814ff6deac1349df5d7779132c9e253
Elias F Martins <eliasmartinsdev8@gmail.com>
2023-10-17 18:39:18 -0300

    feat: create  of the form

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,25 @@
-import Image from "next/image";
 import { PageContainer, PageContent } from "./styles/PageStyles";
+// import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
 export default function Home() {
   return (
     <PageContainer>
       <PageContent>
-        <h2>Bag</h2>
+
+
+    {/* <Swiper
+      spaceBetween={50}
+      slidesPerView={3}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+      ...
+    </Swiper> */}
       </PageContent>
     </PageContainer>
   );
--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -5,6 +5,24 @@ export default function Register() {
     <PageContainer>
       <PageContent>
         <h2>Register</h2>
+        <form action="http://127.0.0.1:8000/filmes/" method="post">
+        <label htmlFor="nameMovie">Isert the name Movie</label>
+          <input type="text" id="nameMovie" name="nameMovie" />
+        <label htmlFor="descibre">Descibre of Movie</label>
+          <input type="text" id="describe" name="describe" />
+        <label htmlFor="theme">Theme of Movie</label>
+          <input type="text" id="theme" name="theme" />
+        <label htmlFor="indication">Indication for movie</label>
+          <input type="text" id="indication" name="indication" />
+        <label htmlFor="relese">Relese</label>
+          <input type="text" id="relese" name="relese" />
+        <label htmlFor="main">Main actors </label>
+          <input type="text" id="main" name="main" />
+          <button type="submit">
+            send the form
+          </button>
+        </form>
+        
       </PageContent>
     </PageContainer>
   );

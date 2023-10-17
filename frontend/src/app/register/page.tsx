626048fcb8252fbde37445f84a12a67eda98f816
Elias F Martins <eliasmartinsdev8@gmail.com>
2023-10-17 19:04:21 -0300

    fixing: convertendo dados para json

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -1,11 +1,40 @@
+'use client'
 import { PageContainer, PageContent } from "../styles/PageStyles";
 
 export default function Register() {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const formData = new FormData(form);
+    const jsonData = {};
+    formData.forEach((value, key) => {
+      jsonData[key] = value;
+      console.log(jsonData)
+    });
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(jsonData),
+    };
+
+    try {
+      const response = await fetch('http://127.0.0.1:8000/filmes/', requestOptions);
+      if (response.ok) {
+        // Lidar com a resposta bem-sucedida do Django, se necessário
+      } else {
+        // Lidar com erros de resposta, se necessário
+      }
+    } catch (error) {
+      console.error('Erro ao enviar a solicitação:', error);
+    }
+  };
   return (
     <PageContainer>
       <PageContent>
         <h2>Register</h2>
-        <form action="http://127.0.0.1:8000/filmes/" method="post">
+        <form onSubmit={handleSubmit} method="post">
         <label htmlFor="nameMovie">Isert the name Movie</label>
           <input type="text" id="nameMovie" name="nameMovie" />
         <label htmlFor="descibre">Descibre of Movie</label>

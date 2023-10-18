019f44d73d188c4448526fb32b29f53410a7b922
Elias F Martins <eliasmartinsdev8@gmail.com>
2023-10-18 18:52:27 -0300

    feat: puxando dados do back-end

--- frontend/src/app/movies/page.tsx
+++ frontend/src/app/movies/page.tsx
@@ -1,19 +1,47 @@
+'use client'
+import { useEffect, useState } from "react";
+
 import { Card } from "../components/Card";
 import { MovieContent, MoviesContainer } from "./styled";
 export default function Movies() {
+  const [movieData, setMovieData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const req = await fetch('http://127.0.0.1:8000/api/filmes/');
+        const reqJson = await req.json();
+        setMovieData(reqJson);
+        console.log(reqJson)
+      } catch (error) {
+
+        console.error('Erro ao buscar os filmes:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
     <MoviesContainer>
       <MovieContent>
 
-        {biscoito.map(card => {
-          return(
+        {movieData ? movieData.map(({id, titulo, descricao, tema, indicacao, estreia, imagem}) => {
+          return (
             <Card
-            img={card.img} descricao={card.descricao} estreia={estreia} indicacao={indicacao} tema={tema} titulo={titulo} key={estreia}
+              key={id}
+              descricao={descricao}
+              titulo={titulo}
+              tema={tema}
+              indicacao={indicacao}
+              estreia={estreia}
+              img={imagem}
             />
           )
-        })}
-        
-            </MovieContent>
+        }) : <h2>
+          nenhum filme cadastrado
+        </h2>}
+
+      </MovieContent>
     </MoviesContainer>
   )
 }

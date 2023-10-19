60ecd7f3f64e805ec83e21359b3de47c8a8c4ae6
Elias F Martins <eliasmartinsdev8@gmail.com>
2023-10-19 20:20:20 -0300

    style: ajustando styled movies and card

--- frontend/src/app/movies/styled.ts
+++ frontend/src/app/movies/styled.ts
@@ -1,16 +1,19 @@
 'use client'
-import styled from "styled-components"
+import styled from 'styled-components'
 
 export const MoviesContainer = styled.main`
-display: flex;
-align-items: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `
 export const MovieContent = styled.div`
-min-height: 55vh;
-max-width: 1200px;
-width: 100%;
-margin: 0 auto;
-align-items: center;
-justify-content: center;
-display: flex;
+  padding: 25px;
+  min-height: 55vh;
+  max-width: 1200px;
+  width: 100%;
+  margin: 0 auto;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  display: flex;
 `

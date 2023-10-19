1ad8fb9a128b2ad8a0fc3547ca41c5ca84fd4957
Elias F Martins <eliasmartinsdev8@gmail.com>
2023-10-19 18:46:57 -0300

    fixing: ajustando inputs select e options

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -1,15 +1,33 @@
 'use client'
+import { SyntheticEvent } from "react";
 import { PageContainer, PageContent } from "../styles/PageStyles";
+interface FormState {
+  titulo: string;
+  descricao: string;
+  temas: string;
+  indicacao:string;
+  estreia: string;
+  atores_principais: string;
+  imagem: string;
+}
 
 export default function Register() {
-  const handleSubmit = async (e) => {
+  const initialFormState: FormState ={
+    titulo:"",
+    descricao:"",
+    temas: "",
+    indicacao:"",
+    estreia:"",
+    atores_principais:"",
+    imagem: "",
+  }
+  const handleSubmit = async (e: SyntheticEvent) => {
     e.preventDefault();
-    const form = e.target;
+    const form = e.target as HTMLFormElement;
     const formData = new FormData(form);
-    const jsonData = {};
+    const jsonData : FormState = {} as FormState;
     formData.forEach((value, key) => {
-      jsonData[key] = value;
-      console.log(jsonData)
+      jsonData[key as keyof FormState] = value as string;
     });
     const requestOptions = {
       method: 'POST',
@@ -39,19 +57,41 @@ export default function Register() {
           <label htmlFor="titulo">Isert the name Movie</label>
           <input type="text" id="titulo" name="titulo" />
           <label htmlFor="descricao">Descibre of Movie</label>
-          <input type="text" id="descricao" name="descricao" />
-          <label htmlFor="tema">Theme of Movie</label>
-          <input type="text" id="tema" name="tema" />
-          <label htmlFor="indicacao">Indication for movie</label>
-          <input type="text" id="indicacao" name="indicacao" />
+          <textarea name="descricao" id="descricao" cols={30} rows={10}>
+            digita aqui
+          </textarea>
+          <label htmlFor="temas">Escola um tema:</label>
+          <select name="temas" id="temas">
+            <option value="Acao">Ação</option>
+            <option value="Aventura">Aventura</option>
+            <option value="Comedia">Comédia</option>
+            <option value="Drama">Drama</option>
+            <option value="Ficcao Cientifica">Ficção Cienífica</option>
+            <option value="Guerra">Guerra</option>
+            <option value="Romance">Romance</option>
+            <option value="Terror">Terror</option>
+            <option value="Animacao">Animação</option>
+            <option value="Musical">Musical</option>
+            <option value="Crime">Crime</option>
+            <option value="Historia Real">História Real</option>
+
+          </select>
+          <label htmlFor="indicacao">Escolha uma indicação:</label>
+          <select name="indicacao" id="indicacao">
+            <option value="Livre">Livre</option>
+            <option value="10+">10+</option>
+            <option value="12+">12+</option>
+            <option value="14+">14+</option>
+            <option value="16+">16+</option>
+            <option value="18+">18+</option>
+          </select>
           <label htmlFor="estreia">Relese</label>
           <input type="date" id="estreia" name="estreia" />
           <label htmlFor="atores_principais">Main actors</label>
           <input type="text" id="atores_principais" name="atores_principais" />
-          <label htmlFor="likes">likes </label>
-          <input type="number" id="likes" name="likes" />
-          <label htmlFor="deslikes">deslikes</label>
-          <input type="number" id="deslikes" name="deslikes" />
+          <label htmlFor="imagem">Link imagem para o filme </label>
+          <input type="text" id="imagem" name="imagem" />
+
 
           <button type="submit">
             send the form

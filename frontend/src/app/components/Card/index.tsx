4f4247a6070cf28bf29b0b1bf94ac511836ad055
Elias F Martins <eliasmartinsdev8@gmail.com>
2023-10-21 15:51:33 -0300

    feat: add função de update nos card

--- frontend/src/app/components/Card/index.tsx
+++ frontend/src/app/components/Card/index.tsx
@@ -1,22 +1,74 @@
-import { title } from "process";
+/* eslint-disable @next/next/no-img-element */
 import { CardContainer, CardContent } from "./styled";
-import Image from "next/image";
-import Link from "next/link";
 
 export interface CardProps {
-  img: string;
+  id: string
   titulo: string;
   descricao: string;
   tema: string;
-  estreia: string;
   indicacao: string;
+  estreia: string;
+  url_imagem: string;
+  atores_principais: string;
+  reload: () => void;
 }
 
 
-export const Card = ({ id,img, titulo, descricao, tema, estreia, indicacao }: CardProps) => {
+export const Card = ({ id, atores_principais, url_imagem, titulo, descricao, tema, estreia, indicacao, reload }: CardProps) => {
+  const deleteCard = async () => {
+    try {
+      const response = await fetch(`http://127.0.0.1:8000/api/filmes/${id}/`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (response.status === 204) {
+        // A exclusão foi bem-sucedida, você pode realizar alguma ação aqui
+        // Por exemplo, atualizar o estado da sua aplicação para refletir a exclusão
+        reload()
+      } else {
+        console.log('Deu Ruim Aqui meu Bom ')
+      }
+    } catch (error) {
+      console.error('Erro ao excluir o card:', error);
+    }
+  }
+  const updateCard = async () => {
+    try {
+      const updateData = {
+        titulo: `${id}`,
+        descricao: `${id}`,
+        tema: "Acao",
+        indicacao: "Livre",
+        estreia: "2023-10-21",
+        atores_principais: `${id}`,
+        url_imagem: `${id}`,
+      }
+
+      const response = await fetch(`http://127.0.0.1:8000/api/filmes/${id}/`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(updateData)
+      });
+
+      if (response.status === 200) {
+        // A exclusão foi bem-sucedida, você pode realizar alguma ação aqui
+        // Por exemplo, atualizar o estado da sua aplicação para refletir a exclusão
+        reload()
+      } else {
+        console.log('Deu Ruim Aqui meu Bom ')
+      }
+    } catch (error) {
+      console.error('Erro ao atualizar o card o card:', error);
+    }
+  }
   return (
     <CardContainer>
-      <img src={`${img}`} alt="nada" />
+      <img src={`${url_imagem}`} alt="imagem Filme" />
       <CardContent>
         <h2>{titulo}</h2>
         <p>{descricao}</p>
@@ -28,9 +80,13 @@ export const Card = ({ id,img, titulo, descricao, tema, estreia, indicacao }: Ca
             {estreia}
           </span>
         </div>
-        <Link href={`/edit${id}`}>
-        <button>Edit Card</button>
-        </Link>
+        <div>
+          <button onClick={updateCard}>Edit Card</button>
+
+          <button onClick={deleteCard}>Delete Card</button>
+
+
+        </div>
       </CardContent>
 
     </CardContainer>
--- frontend/src/app/movies/page.tsx
+++ frontend/src/app/movies/page.tsx
@@ -6,20 +6,21 @@ import { MovieContent, MoviesContainer } from "./styled";
 export default function Movies() {
   const [movieData, setMovieData] = useState([]);
 
+  const fetchData = async () => {
+    try {
+      const req = await fetch('http://127.0.0.1:8000/api/filmes/');
+      const reqJson = await req.json();
+      setMovieData(reqJson);
+      console.log(reqJson)
+    } catch (error) {
+
+      console.error('Erro ao buscar os filmes:', error);
+    }
+  };
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const req = await fetch('http://127.0.0.1:8000/api/filmes/');
-        const reqJson = await req.json();
-        setMovieData(reqJson);
-        console.log(reqJson)
-      } catch (error) {
-
-        console.error('Erro ao buscar os filmes:', error);
-      }
-    };
-
     fetchData();
+
+
   }, []);
   return (
     <MoviesContainer>
@@ -28,13 +29,16 @@ export default function Movies() {
         {movieData.length > 0 ? movieData.map(({ id, titulo, descricao, tema, indicacao, estreia, url_imagem }) => {
           return (
             <Card
+              atores_principais='none'
               key={id}
+              id={id}
               descricao={descricao}
               titulo={titulo}
               tema={tema}
               indicacao={indicacao}
               estreia={estreia}
-              img={url_imagem}
+              url_imagem={url_imagem}
+              reload={fetchData}
             />
           )
         }) : <h2>

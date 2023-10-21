ce2df5917f89f4c049d90cfd79451316891f1acc
Gabriel Gontijo <gontijogabr@gmail.com>
2023-10-21 13:27:30 -0300

    update

--- backend/filmesflix/views.py
+++ backend/filmesflix/views.py
@@ -1,7 +1,10 @@
 from rest_framework import viewsets
-from filmesflix.models import Filmes
-from filmesflix.serializers import FilmesSerializer
-
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Filmes
+from django.http import HttpResponse
+from .serializers import FilmesSerializer
 
 class FilmesViewSet(viewsets.ModelViewSet):
     queryset = Filmes.objects.all()

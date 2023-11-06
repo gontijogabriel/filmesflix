433cd2f3268a3b2478ccecc35a11cd00e0718499
Gabriel Gontijo <gontijogabr@gmail.com>
2023-11-06 11:25:44 -0300

    trocando url do swgger e redoc

--- backend/setup/urls.py
+++ backend/setup/urls.py
@@ -20,6 +20,6 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('filmesflix.urls')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
